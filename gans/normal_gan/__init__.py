143ad263ada6d16502f0b9c27947aa845a4c9363
caron14 <supika.caron@gmail.com>
2023-01-05 23:32:13 +0900

    first commit

---
+++ gans/normal_gan/__init__.py
@@ -0,0 +1,11 @@
+from normal_gan.ngan import (
+    Generator_NGAN,
+    Discriminator_NGAN,
+)
+from normal_gan.losses_ngan import (
+    generator_loss,
+    discriminator_loss,
+)
+from normal_gan.train_ngan import (
+    train_ngan,
+)

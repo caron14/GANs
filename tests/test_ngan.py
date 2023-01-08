43c634eb3dc56ac956b71be8732b2df241c72770
caron14 <supika.caron@gmail.com>
2023-01-08 15:41:36 +0900

    implemented test_Discriminator_NGAN()

--- tests/test_ngan.py
+++ tests/test_ngan.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 
 from gans.normal_gan.ngan import Generator_NGAN
+from gans.normal_gan.ngan import Discriminator_NGAN
 
 
 
@@ -36,6 +37,38 @@ def test_Generator_NGAN():
     assert tuple(test_output.shape) == (n_samples, output_dim)
     test_output = final_block(test_noise)
     assert tuple(test_output.shape) == (n_samples, output_dim)
+
+
+def test_Discriminator_NGAN():
+    """
+    Test for the discriminator of Normal GAN
+    """
+    input_dim, hiddlen_dim = 1000, 100
+    disc = Discriminator_NGAN(image_dim=input_dim, hidden_dim=hiddlen_dim)
+    assert len(disc.disc) == 4  # There are the 4 blocks
+
+    # Check the layer size and type
+    ## hidden-layer block
+    hidden_block = disc.block(input_dim, hiddlen_dim)
+    assert len(hidden_block) == 2
+    assert type(hidden_block[0]) == nn.Linear
+    assert type(hidden_block[1]) == nn.LeakyReLU
+    ## final-layer block
+    final_block = disc.block(input_dim, 1, final_layer=True)
+    assert len(final_block) == 1
+    assert type(final_block[0]) == nn.Linear
+
+    # Check the output size
+    ## create the test noise(input)
+    n_samples, noise_dim = 5000, input_dim
+    test_noise = torch.randn(n_samples, noise_dim)
+    ## generate the test image(output)
+    test_output = hidden_block(test_noise)
+    assert tuple(test_output.shape) == (n_samples, hiddlen_dim)
+    test_output = final_block(test_noise)
+    assert tuple(test_output.shape) == (n_samples, 1)
+    test_output = disc(test_noise)
+    assert tuple(test_output.shape) == (n_samples, 1)
 
 
 

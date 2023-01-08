3bbe48101a902428e2fb728b967d466fbff6187d
caron14 <supika.caron@gmail.com>
2023-01-08 15:31:59 +0900

    specified the PATHs from the main.py path

--- gans/normal_gan/__init__.py
+++ gans/normal_gan/__init__.py
@@ -1,11 +1,11 @@
-from normal_gan.ngan import (
+from gans.normal_gan.ngan import (
     Generator_NGAN,
     Discriminator_NGAN,
 )
-from normal_gan.loss_func_ngan import (
+from gans.normal_gan.loss_func_ngan import (
     generator_loss,
     discriminator_loss,
 )
-from normal_gan.train_ngan import (
+from gans.normal_gan.train_ngan import (
     train_ngan,
 )

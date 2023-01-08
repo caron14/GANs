e7a259a3837cef23af56a836c47e16fca8dbae17
caron14 <supika.caron@gmail.com>
2023-01-08 20:33:17 +0900

    updated

---
+++ gans/__init__.py
@@ -0,0 +1,8 @@
+from gans.helper_functions import (
+    create_noise,
+    show_images,
+)
+from gans.loss_func_gans import (
+    generator_loss,
+    discriminator_loss,
+)
--- gans/normal_gan/__init__.py
+++ gans/normal_gan/__init__.py
@@ -2,10 +2,6 @@ from gans.normal_gan.ngan import (
     Generator_NGAN,
     Discriminator_NGAN,
 )
-from gans.normal_gan.loss_func_ngan import (
-    generator_loss,
-    discriminator_loss,
-)
 from gans.normal_gan.train_ngan import (
     train_ngan,
 )

01beada88a3980cc1d1dae332a59563dd541d138
caron14 <supika.caron@gmail.com>
2025-06-01 00:49:20 +0900

    add execution script and comment out image display in utils.py

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# Execution Bash script
+# Activate Python virtual environment if needed (uncomment below)
+source .venv/bin/activate
+
+# Run the main script
+python main.py
+
+# If you want to run another entry point, use below
+# python torch_gans/train.py
